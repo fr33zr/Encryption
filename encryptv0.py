94fcc6f3618b1f5690eb26e739f67a9414127312
fr33zr <47411492+fr33zr@users.noreply.github.com>
2019-02-24 13:20:15 -0800

    Update encryptv0.py
    Added additional exit messages.

--- encryptv0.py
+++ encryptv0.py
@@ -80,6 +80,7 @@ if HEX_.lower() == 'yes':
 	HEX_ = input("Input your string/quote here for HEX Encryption >  ")
 	print (crypt.crypt(HEX_, "HX"))
 else:
+	print ("You entered no for all of the questions.")
 	print ("Goodbye!!!")
 sys.exit(0)
 
